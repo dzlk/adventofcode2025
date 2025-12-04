392ad345811da2957af7580b5e35c7f224e151fc
Evgeny <dlb.diesel@gmail.com>
2025-12-04 15:54:08 +0500

    Run formatting

--- scripts/base_day.gd
+++ scripts/base_day.gd
@@ -10,54 +10,62 @@ var title: String = "Unknown"
 
 var file: String
 
+
 func _init() -> void:
 	logger = Lggr.new()
 
+
 func _ready() -> void:
 	HUD.set_title(title)
-	
+
 	# Run tests and then solve with real input
-	await get_tree().process_frame  # Wait for UI to be ready
-	
+	await get_tree().process_frame # Wait for UI to be ready
+
 	run_solutions()
+
 
 func solve_part_one(input: String) -> Variant:
 	logger.error("solve_part_one not implemented")
 	logger.debug(input)
 	return 0
-	
+
+
 func solve_part_two(input: String) -> Variant:
 	logger.error("solve_part_one not implemented")
 	logger.debug(input)
 	return 0
-	
+
+
 func input_to_lines(input: String, allow_empty: bool = true) -> PackedStringArray:
 	return input.split("\n", allow_empty)
-	
+
+
 func run_solutions() -> void:
 	var file_path = Globals.get_day_dir(day) + file
 	var input = Globals.read_file(file_path)
-	
+
 	if input.is_empty():
 		logger.error("file not found. Place your puzzle input in: %s" % file_path)
 		return
-		
-	logger.info("Solving...", )
+
+	logger.info("Solving...")
 	logger.log_delim(logger.Level.INFO, "=", 50)
-	
+
 	var result = solve_part_one(input)
 	logger.clog("ℹ️Part one result: %s" % str(result), Color.AQUAMARINE)
 	update_part_one(result)
-	
+
 	# Solve Part 2
 	result = solve_part_two(input)
 	logger.clog("ℹ️Part two result: %s" % str(result), Color.AQUAMARINE)
 	update_part_two(result)
-	
+
 	logger.log_delim(logger.Level.INFO, "=", 50)
-	
+
+
 func update_part_one(value: Variant):
 	HUD.update_part_one(value)
+
 
 func update_part_two(value: Variant):
 	HUD.update_part_two(value)
--- tests/test_day03.gd
+++ tests/test_day03.gd
@@ -2,36 +2,19 @@ extends GutTest
 
 const Day03 = preload("res://days/day03/day03.gd")
 
+
 func before_each():
 	gut.p("Running test...")
+
 
 func after_each():
 	gut.p("Test completed")
 
+
 func test_sample():
 	var day = Day03.new()
-	
+
 	var line = '745433452452451332445546443'
 	var max_num = 755555546443
-	
-	assert_eq(day.get_max(line, 12), max_num, "Should correct getting max for l=12")
-	
 
-# _ _    _  _  _      _______
-# 7x5xxxx5xx5xx5xxxxxx5546443
-# 74543 34524 52451 33244 55464 43
-# 012345678900123456789
-	"""
-	change_max: 745433452452 (0)
-	change_max: 745133244554 (1)
-	change_max: 745133244443 (9)
-	change_max: 745454334524 (3)
-	change_max: 745454513324 (6)
-	change_max: 745454513343 (10)
-	change_max: 754334524524 (1)
-	change_max: 751332445546 (2)
-	change_max: 754334524524 (2)
-	change_max: 754332445546 (3)
-	change_max: 754333345245 (5)
-	"""
-	
+	assert_eq(day.get_max(line, 12), max_num, "Should correct getting max for l=12")
